8fe3e3509cf5c71f467ac9aec05ab14e209eeced
Juan1110 <100285627+Juan1110@users.noreply.github.com>
2022-04-04 07:12:38 -0500

    main

---
+++ hvjs/assets/css/js/votaciones.js
@@ -0,0 +1,10 @@
+let numero = Math.random() * 5;
+
+console.log("Aleatorio: " + nota)
+
+if (numero >= 3)
+{
+    console.log("paso la materia");
+}else {
+     console.log("perdio la materia");
+ }
