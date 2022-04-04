dfb5493f5273367425ddf8e277392e563d68a906
Juan1110 <100285627+Juan1110@users.noreply.github.com>
2022-04-04 11:11:25 -0500

    main

---
+++ hvjs/assets/css/js/ejercicios.js
@@ -0,0 +1,71 @@
+// let numero = Math.random() * 5;
+
+// console.log("Aleatorio: " + nota);
+
+// if (numero <= 3)
+// {
+//     console.log("perdio la materia");
+// }else if(nota <=  4) {
+//      console.log("muy bien");
+//  }else {
+//      console.log("Excelente");
+//  }
+
+
+// FRUTAS
+
+// let fruta = prompt("Ingrese el nombre de la fruta");
+
+// if(fruta == "Manzana")
+// {
+//     console.log("La Manzana vale $10.000");
+// } else if(fruta == "Pera"){
+//     console.log("La Pera vale $15.000");
+// } else if(fruta == "Limon"){
+//     console.log("El Limon vale $6.000");
+// } else if(fruta == "Mango"){
+//     console.log("El Mango vale $8.000");
+// } else {
+//     console.log("Se nos agoto la fruta");
+// } 
+
+// switch(fruta){
+//     case "Manzana":
+//         alert("La Manzana vale $10.000");
+//         break;
+//     case "Pera":
+//         alert("La Pera vale $15.000");
+//         break
+//     case "Limon":
+//         alert("El Limon vale $6.000");
+//         break    
+//     case "Mango":
+//         alert("El Mango vale $8.000");
+//         break;
+//     default:
+//         alert("Se nos agoto la fruta");
+// }
+
+
+// DINERO PARA PROYECTOS DEL ESTADO
+
+// let CantidadDeProyectos = 0;
+// let dineroEnCaja = Number(prompt("Ingrese el monto de dinero"));
+
+// while(dineroEnCaja > 0)
+// {
+//     let presupuestoProyecto = Number(prompt("Ingrese el monto del proyecto"));
+
+//     if(dineroEnCaja >= presupuestoProyecto)
+//     {
+//         alert("Se asignaron recursos para su proyecto");
+//         CantidadDeProyectos ++;
+//         dineroEnCaja -= presupuestoProyecto;
+//     }else{
+//         alert("El monto de su proyecto sobrepasa el presupuesto");
+//     }
+    
+// }
+
+// alert("Se ha(n) aprobado: " + CantidadDeProyectos + " proyecto(s)");
+
--- hvjs/assets/css/js/votaciones.js
+++ hvjs/assets/css/js/votaciones.js
@@ -1,92 +1,78 @@
-// let numero = Math.random() * 5;
+let candidato1 = 0;
+let candidato2 = 0;
+let candidato3 = 0;
+let candidato4 = 0;
 
-// console.log("Aleatorio: " + nota);
+let voto;
 
-// if (numero <= 3)
-// {
-//     console.log("perdio la materia");
-// }else if(nota <=  4) {
-//      console.log("muy bien");
-//  }else {
-//      console.log("Excelente");
-//  }
+for(let i = 1; i < 13; i++)
+{ 
+    voto = prompt("Ingrese el número del candidato de su preferencia: \n1. Mateo. \n2. Mateo. \n3. Lucas. \n4. Juan.");
 
-// console.log("-----------------------")
-
-// let fruta = prompt("Ingrese el nombre de la fruta");
-
-// if(fruta == "Manzana")
-// {
-//     console.log("La Manzana vale $10.000");
-// } else if(fruta == "Pera"){
-//     console.log("La Pera vale $15.000");
-// } else if(fruta == "Limon"){
-//     console.log("El Limon vale $6.000");
-// } else if(fruta == "Mango"){
-//     console.log("El Mango vale $8.000");
-// } else {
-//     console.log("Se nos agoto la fruta");
-// } 
-
-// switch(fruta){
-//     case "Manzana":
-//         alert("La Manzana vale $10.000");
-//         break;
-//     case "Pera":
-//         alert("La Pera vale $15.000");
-//         break
-//     case "Limon":
-//         alert("El Limon vale $6.000");
-//         break    
-//     case "Mango":
-//         alert("El Mango vale $8.000");
-//         break;
-//     default:
-//         alert("Se nos agoto la fruta");
-// }
-
-// for(let i = 1; i < 30; i++)
-// {
-//     console.log(i);
-// }
-// let candidato1 = 0;
-// let candidato2 = 0;
-// let candidato3 = 0;
-// let candidato4 = 0;
-
-// let voto = prompt("Ingrese el número del candidato de su preferencia: \n1. Cantidato 1. \n2. Cantidato 2. \n3. Cantidato 3. \n4. Cantidato 4.");
-
-// switch(voto){
-//     case "1":
-//         candidato1++;
-//         break;
-//     case "2":
-//         candidato2++;
-//         break;
-//     case "3":
-//         candidato3++;
-//         break;
-//     case "4":
-//         candidato4++;
-//         break;
-
-// }
-let CantidadDeProyectos = 0;
-let dineroEnCaja = Number(prompt("Ingrese el monto de dinero"));
-
-while(dineroEnCaja > 0)
-{
-    let presupuestoProyecto = Number(prompt("Ingrese el monto del proyecto"));
-
-    if(dineroEnCaja >= presupuestoProyecto)
-    {
-        alert("Se asignaron recursos para su proyecto");
-        CantidadDeProyectos ++;
-        dineroEnCaja -= presupuestoProyecto;
-    }else{
-        alert("El monto de su proyecto sobrepasa el presupuesto");
+switch(voto) {
+    case "1":
+        candidato1++;
+        break;
+    case "2":
+        candidato2++;
+        break;
+    case "3":
+        candidato3++;
+        break;
+    case "4":
+        candidato4++;
+        break;
+    default:
+        voto ++;
+        alert("Voto inválido, ingrese nuevamente su voto");
     }
-    
 }
 
-alert("Se ha(n) aprobado: " + CantidadDeProyectos + " proyecto(s)");
+
+$mensaje = "";
+if(candidato1 > candidato2 && candidato1 > candidato3 && candidato1 > candidato4)
+{
+    $mensaje += "Mateo";
+}
+
+else if (candidato2 > candidato1 && candidato2 > candidato3 && candidato2 > candidato4)
+{
+    $mensaje += "Marcos";
+}
+else if (candidato3 > candidato1 && candidato3 > candidato2 && candidato3 > candidato4)
+{
+    $mensaje += "Lucas";
+}
+else if (candidato4 > candidato1 && candidato4 > candidato2 && candidato4 > candidato3)
+{
+    $mensaje += "Juan";
+}
+else
+{
+    $mensaje = "No hay ganador";
+}
+
+alert($mensaje);
+
+
+// DINERO PARA PROYECTOS DEL ESTADO
+
+// let CantidadDeProyectos = 0;
+// let dineroEnCaja = Number(prompt("Ingrese el monto de dinero"));
+
+// while(dineroEnCaja > 0)
+// {
+//     let presupuestoProyecto = Number(prompt("Ingrese el monto del proyecto"));
+
+//     if(dineroEnCaja >= presupuestoProyecto)
+//     {
+//         alert("Se asignaron recursos para su proyecto");
+//         CantidadDeProyectos ++;
+//         dineroEnCaja -= presupuestoProyecto;
+//     }else{
+//         alert("El monto de su proyecto sobrepasa el presupuesto");
+//     }
+    
+// }
+
+// alert("Se ha(n) aprobado: " + CantidadDeProyectos + " proyecto(s)");
